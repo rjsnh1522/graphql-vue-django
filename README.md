18ed3bdc51b44b873457dc0497db9ed4f7f9e5ee
Pawan Chaurasia <logan@Pawans-MacBook-Pro.local>
2019-06-18 07:04:28 +0530

    update track mutation

--- README.md
+++ README.md
@@ -10,3 +10,59 @@ Add ElasticSearch
 
 Implement django-channels
 
+```
+{
+  tracks {
+    id
+    title
+    description
+  }
+}
+
+
+mutation {
+  tokenAuth(username:"Reed", password:"123456789"){
+    token
+    
+  }
+}
+
+{
+  user(id:1){
+    id
+    password
+    username
+    email
+  }
+}
+
+
+
+mutation {
+  createUser(username: "Reed", password:"123456789",
+  email:"reed@yahoo.com"){
+    user{
+      id
+      password
+      email
+      username
+    }
+    
+  }
+}
+
+
+mutation {
+  createTrack(title: "Track 5", 
+    					description: "Track 5 Description",
+  						url:"https://track5.com"){
+    track{
+      id
+      title
+      description
+      url
+      createdAt
+    }
+  }
+}
+```
--- server_gql_django/tracks/schema.py
+++ server_gql_django/tracks/schema.py
@@ -35,6 +35,35 @@ class CreateTrack(graphene.Mutation):
         return CreateTrack(track)
 
 
+class UpdateTrack(graphene.Mutation):
+    track = graphene.Field(TrackType)
+
+    class Arguments:
+        track_id = graphene.Int(required=True)
+        title = graphene.String()
+        description = graphene.String()
+        url = graphene.String()
+
+    def mutate(self, info, **kwargs):
+        track_id = kwargs.get('track_id')
+        title = kwargs.get('title')
+        url = kwargs.get('url')
+        description = kwargs.get('description')
+
+        user = info.context.user
+        track = Track.objects.get(id=track_id)
+        if track.posted_by != user:
+            raise Exception('Not permitted to update this track')
+        track.title = title
+        track.description = description
+        track.url = url
+        track.save()
+
+        return UpdateTrack(track=track)
+
+
+
 class Mutation(graphene.ObjectType):
     create_track = CreateTrack.Field()
+    update_track = UpdateTrack.Field()
 
